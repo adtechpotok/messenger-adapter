281acb20e53cc2ce63b33d858c789e51f82b3318
Roman Olegovich <r.minatulaev@rambler-co.ru>
2018-09-21 19:19:50 +0300

    Remove usage RoutingKeyItem

--- Event/Events.php
+++ Event/Events.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Enqueue\MessengerAdapter\Event;
 
 abstract class Events
@@ -17,4 +8,5 @@ abstract class Events
     public const ENVELOPE_EXECUTE_FAIL = 'ENVELOPE_EXECUTE_FAIL';
     public const ENVELOPE_REACH_REPEAT_LIMIT = 'ENVELOPE_REACH_REPEAT_LIMIT';
     public const ENVELOPE_FAIL_ON_REPEAT = 'ENVELOPE_FAIL_ON_REPEAT';
+    public const ON_SEND_MESSAGE = 'ON_SEND_MESSAGE';
 }
